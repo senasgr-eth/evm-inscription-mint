bc08cb3b5ef41f46f83b1d9f6889ee08d9014b01
Sena Sagara <39322822+senasgr-eth@users.noreply.github.com>
2023-11-27 10:05:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,27 @@
 const config = {
-    // 你想要打多少张，这里就设置多少，建议单次别超过 50，不然容易不上链
-    repeatCount: 1,
+    // Set the number you want to mint here. It is recommended not to exceed 50 at a time, otherwise it may not be successfully recorded on the chain.
 
-    // 在当前的 gas 基础上增加多少倍
+    repeatCount: 10,
+
+    // Increase by how many times on the current gas base.
     increaseGas: 1.2,
 
-    // 你钱包的私钥
-    privateKey: "",
+    // The private key of your walle
+    privateKey: "*****",
+    sendaddr: "0x00000",
 
-    // 铭文json数据（替换成你想打的铭文json格式数据）
-    tokenJson: 'data:,{"p":"fair-20","op":"mint","tick":"fair","amt":"1000"}',
+    // inscription JSON data (replace with the JSON-formatted data of the runes you want to engrave)
+    tokenJson: 'data:,{"a":"NextInscription","p":"oprc-20","op":"mint","tick":"NI","amt":"10000"}',
 
     // RPC结点（兼容 evm 链都行）打哪条链就用哪条链的节点地址
     // eth =>  https://mainnet.infura.io/v3
     // arb => https://arb1.arbitrum.io/rpc
-    // polygon => https://polygon-rpc.com
+    polygon => https://polygon-rpc.com
     // op => https://mainnet.optimism.io
     // linea => https://mainnet.infura.io/v3
     // scroll => https://rpc.scroll.io
     // zks => https://mainnet.era.zksync.io
-    rpcUrl: "https://arb1.arbitrum.io/rpc"
+    //rpcUrl: "https://arb1.arbitrum.io/rpc"
 }
 
 module.exports = config
